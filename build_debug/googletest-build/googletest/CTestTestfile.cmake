6df79af34cc7448c277eb9dadf01848e633fc83d
bbddbb1 <2454213563@qq.com>
2022-09-01 20:37:20 +0800

    build

---
+++ build_debug/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ecd_hyb_1771632295832877/Desktop/code/cmu15445/build_debug/googletest-src/googletest
+# Build directory: /home/ecd_hyb_1771632295832877/Desktop/code/cmu15445/build_debug/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
