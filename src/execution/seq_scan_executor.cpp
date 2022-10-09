29cbbbc54978898d1a7dd28ef7b96274e6ebc611
bbddbb1 <2454213563@qq.com>
2022-10-09 16:00:47 +0800

    seq_scan

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -14,10 +14,30 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {
+}
 
-void SeqScanExecutor::Init() {}
+void SeqScanExecutor::Init() {
+    table_info_ = exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid());
+    iter_ = table_info_->table_->Begin(exec_ctx_->GetTransaction());
+}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { 
+    auto table_end = table_info_->table_->End();
+    for(; iter_ != table_end; iter_++){
+        *tuple = *iter_;
+        *rid = tuple->GetRid();
+        if(plan_->GetPredicate() != nullptr){
+            if (plan_->GetPredicate()->Evaluate(tuple, GetOutputSchema()).GetAs<bool>()){
+                iter_++;
+                return true;
+            }
+        }else{
+            iter_++;
+            return true;
+        }
+    }
+    return false;
+}
 
 }  // namespace bustub
